ceb04e44f50761a42d9d85d5a1d7e20b33206980
Steve <uplneinak@gmail.com>
2023-11-28 07:11:27 +0000

    Remove analytics

--- src/services/Tracking.ts
+++ src/services/Tracking.ts
@@ -1,16 +1,15 @@
 import Constants, { ExecutionEnvironment } from "expo-constants";
-import analytics from '@react-native-firebase/analytics';
 
 const isInProduction = true; //Constants.executionEnvironment === ExecutionEnvironment.Standalone;
 
 export const InitTracking = async () => {
-  await analytics().logEvent("test_analytics_event", {
-    additionaParam: "test",
-  });
+  // await analytics().logEvent("test_analytics_event", {
+  //   additionaParam: "test",
+  // });
 }
 
 export const TrackingEvent = async (eventTitle: string, eventObject: Record<string, string>) => {
   if (isInProduction) {
-    await analytics().logEvent("some_event", eventObject);
+    // await analytics().logEvent("some_event", eventObject);
   }
 };
